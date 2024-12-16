bb2e534236fc61b8f8c78cf2e9794c52e2d167e8
htiennv <tien.nv291997@gmail.com>
2024-12-16 14:04:30 +0700

    Remove unwrap

--- src/client.rs
+++ src/client.rs
@@ -42,7 +42,7 @@ impl Client {
     // GET:/blockflow/blocks
     pub async fn get_blocks(&self, from_ts: u64, to_ts: u64) -> Result<BlocksPerTimesStampRange> {
         let endpoint = format!("/blockflow/blocks?fromTs={}&toTs={}", from_ts, to_ts);
-        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint)).unwrap();
+        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint))?;
         let response = self.inner.get(url).send().await?.json().await?;
         Ok(response)
     }
@@ -55,7 +55,7 @@ impl Client {
         to_ts: u64,
     ) -> Result<BlocksAndEventssPerTimesStampRange> {
         let endpoint = format!("/blockflow/blocks-with-events?fromTs={}&toTs={}", from_ts, to_ts);
-        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint)).unwrap();
+        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint))?;
         let response = self.inner.get(url).send().await?.json().await?;
         Ok(response)
     }
@@ -64,7 +64,7 @@ impl Client {
     // GET:/blockflow/blocks/{block_hash}
     pub async fn get_block(&self, block_hash: &BlockHash) -> Result<BlockEntry> {
         let endpoint = format!("/blockflow/blocks/{}", block_hash);
-        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint)).unwrap();
+        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint))?;
         let response = self.inner.get(url).send().await?.json().await?;
         Ok(response)
     }
@@ -73,7 +73,7 @@ impl Client {
     // GET:/blockflow/blocks-with-events/{block_hash}
     pub async fn get_block_and_events(&self, block_hash: &BlockHash) -> Result<BlockAndEvents> {
         let endpoint = format!("/blockflow/blocks-with-events/{}", block_hash);
-        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint)).unwrap();
+        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint))?;
         let response = self.inner.get(url).send().await?.json().await?;
         Ok(response)
     }
@@ -82,7 +82,7 @@ impl Client {
     // GET:/blockflow/headers/{block_hash}
     pub async fn get_block_header(&self, block_hash: &BlockHash) -> Result<BlockHeaderEntry> {
         let endpoint = format!("/blockflow/headers/{}", block_hash);
-        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint)).unwrap();
+        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint))?;
         let response = self.inner.get(url).send().await?.json().await?;
         Ok(response)
     }
@@ -91,7 +91,7 @@ impl Client {
     // GET:/transactions/details/{txId}
     pub async fn get_transaction(&self, tx_id: String) -> Result<Transaction> {
         let endpoint = format!("/transactions/details/{}", tx_id);
-        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint)).unwrap();
+        let url = Url::parse(&format!("{}/{}", self.base_url, endpoint))?;
         let response = self.inner.get(url).send().await?.json().await?;
         Ok(response)
     }
