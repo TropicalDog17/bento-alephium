c8005dd209a84692c365dd92a66dd193913df4b0
Tuan Tran <tropicaldog17@gmail.com>
2025-01-19 14:23:21 +0700

    remove enum_dispatch

--- src/processors/mod.rs
+++ src/processors/mod.rs
@@ -2,12 +2,10 @@ pub mod default_processor;
 
 
 use async_trait::async_trait;
-use enum_dispatch::enum_dispatch;
 use crate::types::{BlockAndEvents, BlocksAndEventsPerTimestampRange, Transaction};
 use std::fmt::Debug;
 
 #[async_trait]
-#[enum_dispatch]
 pub trait ProcessorTrait: Send + Sync + Debug {
     async fn process_blocks_and_events(&self,blocks_and_events: BlocksAndEventsPerTimestampRange) -> Result<(), anyhow::Error>;
 }
