01cfe147d5180fa9e6af22933fd0542f230a5308
nayy <emmanuelgiftnaomi@gmail.com>
2025-01-31 23:30:41 +0100

    Feat(docs):add docs to contracts

--- src/db.rs
+++ src/db.rs
@@ -7,13 +7,20 @@ use std::error::Error;
 pub type DbPool = r2d2::Pool<r2d2::ConnectionManager<PgConnection>>;
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 pub type DbError = Box<dyn Error + Send + Sync + 'static>;
-
+/// Initializes and returns a connection pool for the PostgreSQL database.
+///
+/// # Panics
+/// This function panics if the `DATABASE_URL` environment variable is missing
+/// or if the connection pool cannot be created.
 pub fn initialize_db_pool() -> DbPool {
     let database_url = env::var("DATABASE_URL").expect("missing DATABASE_URL in .env");
     let manager = r2d2::ConnectionManager::<PgConnection>::new(database_url);
     r2d2::Pool::builder().build(manager).expect("Failed to create pool")
 }
-
+/// Runs pending database migrations.
+///
+/// # Errors
+/// Returns an error if the migration process fails.
 pub fn run_migrations(
     connection: &mut impl MigrationHarness<diesel::pg::Pg>,
 ) -> Result<(), DbError> {
