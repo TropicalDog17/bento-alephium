3f353520d65a47b6ce0e439cb97ca8a57d1d3002
Tuan Tran <tropicaldog17@gmail.com>
2025-02-20 01:28:02 +0700

    fix dotenv

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,8 @@
 use bento_alephium::{api::index::start, config::Config};
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
     println!("Starting server...");
     let config = Config::from_env().await?;
     start(config).await?;
